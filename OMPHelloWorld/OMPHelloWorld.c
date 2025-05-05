ecb9e698b5380bf7c6d3b8d1f81bc5995f4955a8
SongyanLai <laisongyan@foxmail.com>
2025-05-06 00:02:09 +0100

    update

---
+++ OMPHelloWorld/OMPHelloWorld.c
@@ -0,0 +1,30 @@
+/*
+ * OMPHelloWorld.c
+ *
+ *  Created on: 2025Äê5ÔÂ5ÈÕ
+ *      Author: HUAWEI
+ */
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* Hello OpenMP World prints the number of threads and the current thread id
+*/
+int main (int argc, char *argv[]) {
+int numThreads, tid;
+/* This creates a team of threads; each thread has own copy of variables */
+omp_set_num_threads(8);
+#pragma omp parallel private(numThreads, tid) // Parallel Region
+{
+tid = omp_get_thread_num();
+printf("Hello World from thread number %d\n", tid);
+/* The following is executed by the master thread only (tid=0) */
+if (tid == 0)
+{
+numThreads = omp_get_num_threads();
+printf("Number of threads is %d\n", numThreads);
+}
+}
+return 0;
+}
+
